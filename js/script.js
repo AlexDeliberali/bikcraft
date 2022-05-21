c74beabfbeea10138d1e8de80f102cf7412d70fa
Alex Deliberali <37247632+AlexDeliberali@users.noreply.github.com>
2022-05-21 19:12:39 -0300

    Add files via upload
    Adicionado o JavaScript.

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const links = document.querySelectorAll('.header-menu a')
+
+function ativarLink(link) {
+  const url = location.href
+  const href = link.href
+  if (url.includes(href)) {
+    link.classList.add("ativo")
+  }
+}
+
+links.forEach(ativarLink)
+
+//Itens orçamento
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(parametro)
+
+//Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button")
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute("aria-controls")
+  const resposta = document.getElementById(controls)
+  resposta.classList.toggle("ativa")
+  const ativa = resposta.classList.contains("ativa")
+  pergunta.setAttribute("aria-expanded", ativa)
+}
+
+//Galeria de bicicletas
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta)
+}
+perguntas.forEach(eventosPerguntas)
+
+const galeria = document.querySelectorAll(".bicicleta-imagem img");
+const galeriaContainer = document.querySelector(".bicicleta-imagem");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
